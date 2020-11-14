43b4987ebc7b746c5d97aa79fd435738c64ab16c
David Castrillon <david.cast27@gmail.com>
2020-11-14 13:53:34 -0500

    agregar docs

---
+++ docs/precache-manifest.ab2280f357de505c08de7727fbad4c4c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f5e443686b93714d061abb18b502062",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7ac02eea9d5996fe94a0",
+    "url": "/static/css/main.d849ea53.chunk.css"
+  },
+  {
+    "revision": "4fb4b4aa597af662ffc0",
+    "url": "/static/js/2.f1c5b5b3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f1c5b5b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ac02eea9d5996fe94a0",
+    "url": "/static/js/main.55947e56.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
